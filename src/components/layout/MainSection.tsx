5f05237a025589d21f76e11bf753df25209f4f49
AJ1732 <ejemeniboi@gmail.com>
2024-08-06 02:44:21 +0100

    chore: update

--- src/components/layout/MainSection.tsx
+++ src/components/layout/MainSection.tsx
@@ -1,8 +1,10 @@
 const MainSection = () => {
   return (
     <main className="pb-16 pt-4">
-      <section>MainSection</section>
-    </main>
+    <section className="clipped-image">
+      {/* <div className="size-fit bg-white p-5">Hello</div> */}
+    </section>
+  </main>
   );
 };
 export default MainSection;
